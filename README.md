f39cd9933bedf31070cf8533b4b182086ac072e4
shaila-sharmin <52494733+shaila-sharmin@users.noreply.github.com>
2019-08-20 13:15:52 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+Here are the steps that must be performed before running the R script:
+
+1. Download the zip file from this URL.
+2. Unzip the file.
+3. Move ALL of the following files to the SAME DIRECTORY as the R script: features.txt
+    subject_train.txt
+    subject_test.txt
+    X_train.txt
+    X_test.txt
+    y_train.txt
+    y_test.txt
+    
+Once those steps are complete, you can run the R script (run_analysis.R)
+
+The output of the R script is a tidy data set, tidy.csv
---
+++ run_analysis.R
@@ -0,0 +1,51 @@
+
+library(reshape2)
+
+
+subject_train <- read.table("subject_train.txt")
+subject_test <- read.table("subject_test.txt")
+X_train <- read.table("X_train.txt")
+X_test <- read.table("X_test.txt")
+y_train <- read.table("y_train.txt")
+y_test <- read.table("y_test.txt")
+
+
+names(subject_train) <- "subjectID"
+names(subject_test) <- "subjectID"
+
+featureNames <- read.table("features.txt")
+names(X_train) <- featureNames$V2
+names(X_test) <- featureNames$V2
+
+
+names(y_train) <- "activity"
+names(y_test) <- "activity"
+
+
+train <- cbind(subject_train, y_train, X_train)
+test <- cbind(subject_test, y_test, X_test)
+combined <- rbind(train, test)
+
+
+meanstdcols <- grepl("mean\\(\\)", names(combined)) |
+  grepl("std\\(\\)", names(combined))
+
+meanstdcols[1:2] <- TRUE
+
+
+combined <- combined[, meanstdcols]
+
+
+
+
+combined$activity <- factor(combined$activity, labels=c("Walking",
+                                                        "Walking Upstairs", "Walking Downstairs", "Sitting", "Standing", "Laying"))
+
+
+melted <- melt(combined, id=c("subjectID","activity"))
+tidy <- dcast(melted, subjectID+activity ~ variable, mean)
+
+
+write.csv(tidy, "tidy.txt", row.names=FALSE)
+
+
